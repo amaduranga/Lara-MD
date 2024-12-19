20627a3e1c703495f199587d6c4c74eda661de0b
amaduranga <131608716+amaduranga@users.noreply.github.com>
2024-12-20 01:07:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "K7QQTaqT#k0pfnH62Zt_1egvu8tiDG5z7AcMgWJX1TaRquhK4QzE", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
